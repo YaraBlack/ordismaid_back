2e64499d8e97474aa53491ad7883dc7ee4d9490a
YaraBlack <yara26072000@gmail.com>
2026-02-09 00:22:57 +0100

    clean-up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,9 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 import { DataPcService } from './app.service';
 import { DataPcController, HealthController } from './app.controller';
 import { HttpModule } from '@nestjs/axios'
 import { ScheduleModule } from '@nestjs/schedule';
 
-
-// @Module({
-//   imports: [  ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
 
 @Module({
   imports: [
