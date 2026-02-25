e1b6a02d95a685417da7f0d0074ef228bfdb2742
YaraBlack <yara26072000@gmail.com>
2026-02-25 18:21:30 +0100

    add HealthController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WorldStateModule } from './modules/worldState/worldState.module';
+import { HealthController } from './app.controller';
 
 @Module({
   imports: [WorldStateModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
